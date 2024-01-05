dbc627a7eabfbb604d32a4b7647b451d096d9b65
Ixtiyor0919 <Komiljonov2002@outlook.com>
2024-01-06 01:58:54 +0500

    kamchilik biroz yoq boldi

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,61 +1,18 @@
-'use client'
-
-// import React, { useEffect } from "react"
+import React from "react"
 import Hero from "@/components/pages/home/hero"
 import AboutUs from "@/components/pages/home/about-us"
 import Categories from "@/components/pages/home/categories"
 import KeeppInTouch from "@/components/keep-in-touch"
-// import { allProducts } from "@/data"
-// import axios from 'axios'
 import Tranding from "@/components/pages/home/tranding"
-import Exam from "@/components/exm"
 
-export function Home() {
-  // console.log(allProducts)
-  // const fetchData = async () => {
-  //   try {
-  //     // const apiUrl = process.env.REACT_APP_API_URL;
-  
-  //     // if (!apiUrl) {
-  //     //   console.error('API URL is not defined in the environment variables.');
-  //     //   return;
-  //     // }
-  
-  //     // Iterate through each product in allProducts array
-  //     allProducts.forEach(async (product) => {
-  //       const dataToPost = {
-  //         images: product.images,
-  //         category: product.category,
-  //         name: product.name,
-  //         description: product.description,
-  //         price: product.price,
-  //         rating: product.rating,
-  //         createdAt: product.createdAt
-  //       };
-  
-  //       try {
-  //         // Make a separate POST request for each product
-  //         const response = await axios.post('https://16a112ec7cdcde1f.mokky.dev/products', dataToPost);
-          
-  //         console.log(`POST request successful for ${product.name}:`, response.data);
-  //       } catch (error) {
-  //         console.error(`Error making POST request for ${product.name}:`, error);
-  //       }
-  //     });
-  
-  //   } catch (error) {
-  //     console.error('Error in fetchData function:', error);
-  //   }
-  // };
+const Home: React.FC = () => {
   return (
     <>
-    {/* <button onClick={fetchData}>All renders</button> */}
       <Hero />
       <AboutUs />
       <Categories />
       <Tranding />
       <KeeppInTouch />
-    {/* <Exam /> */}
     </>
   )
 }
--- src/components/exm.tsx
+++ src/components/exm.tsx
@@ -49,3 +49,40 @@ function Exam() {
 }
 
 export default Exam;
+
+// console.log(allProducts)
+  // const fetchData = async () => {
+  //   try {
+  //     // const apiUrl = process.env.REACT_APP_API_URL;
+  
+  //     // if (!apiUrl) {
+  //     //   console.error('API URL is not defined in the environment variables.');
+  //     //   return;
+  //     // }
+  
+  //     // Iterate through each product in allProducts array
+  //     allProducts.forEach(async (product) => {
+  //       const dataToPost = {
+  //         images: product.images,
+  //         category: product.category,
+  //         name: product.name,
+  //         description: product.description,
+  //         price: product.price,
+  //         rating: product.rating,
+  //         createdAt: product.createdAt
+  //       };
+  
+  //       try {
+  //         // Make a separate POST request for each product
+  //         const response = await axios.post('https://16a112ec7cdcde1f.mokky.dev/products', dataToPost);
+          
+  //         console.log(`POST request successful for ${product.name}:`, response.data);
+  //       } catch (error) {
+  //         console.error(`Error making POST request for ${product.name}:`, error);
+  //       }
+  //     });
+  
+  //   } catch (error) {
+  //     console.error('Error in fetchData function:', error);
+  //   }
+  // };
